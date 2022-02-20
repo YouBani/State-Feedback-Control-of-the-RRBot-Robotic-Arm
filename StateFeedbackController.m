3801de57c94f184a2382bbf8d726294cf5e8deea
Youness Bani <91743838+YouBani@users.noreply.github.com>
2022-02-20 15:46:55 -0500

    Update StateFeedbackController.m

--- StateFeedbackController.m
+++ StateFeedbackController.m
@@ -28,7 +28,7 @@ x2_dot= theta2_dot;
 x3_dot= (I2*u1 - I2*u2 + m2*r2^2*u1 - m2*r2^2*u2 + l1*m2^2*r2^3*theta1_dot^2*sin(theta2) + l1*m2^2*r2^3*theta2_dot^2*sin(theta2) + I2*g*l1*m1*sin(theta1) + I2*g*m1*r1*sin(theta1) - l1*m2*r2*u2*cos(theta2) + 2*l1*m2^2*r2^3*theta1_dot*theta2_dot*sin(theta2) + l1^2*m2^2*r2^2*theta1_dot^2*cos(theta2)*sin(theta2) - g*l1*m2^2*r2^2*sin(theta1 + theta2)*cos(theta2) + I2*l1*m2*r2*theta1_dot^2*sin(theta2) + I2*l1*m2*r2*theta2_dot^2*sin(theta2) + g*l1*m1*m2*r2^2*sin(theta1) + g*m1*m2*r1*r2^2*sin(theta1) + 2*I2*l1*m2*r2*theta1_dot*theta2_dot*sin(theta2))/(- l1^2*m2^2*r2^2*cos(theta2)^2 + l1^2*m2^2*r2^2 + I2*l1^2*m2 + m1*m2*r1^2*r2^2 + I1*m2*r2^2 + I2*m1*r1^2 + I1*I2);
 x4_dot= -(I2*u1 - I1*u2 - I2*u2 - l1^2*m2*u2 - m1*r1^2*u2 + m2*r2^2*u1 - m2*r2^2*u2 + l1*m2^2*r2^3*theta1_dot^2*sin(theta2) + l1^3*m2^2*r2*theta1_dot^2*sin(theta2) + l1*m2^2*r2^3*theta2_dot^2*sin(theta2) - g*l1^2*m2^2*r2*sin(theta1 + theta2) - I1*g*m2*r2*sin(theta1 + theta2) + I2*g*l1*m1*sin(theta1) + I2*g*m1*r1*sin(theta1) + l1*m2*r2*u1*cos(theta2) - 2*l1*m2*r2*u2*cos(theta2) + 2*l1*m2^2*r2^3*theta1_dot*theta2_dot*sin(theta2) + 2*l1^2*m2^2*r2^2*theta1_dot^2*cos(theta2)*sin(theta2) + l1^2*m2^2*r2^2*theta2_dot^2*cos(theta2)*sin(theta2) - g*l1*m2^2*r2^2*sin(theta1 + theta2)*cos(theta2) - g*m1*m2*r1^2*r2*sin(theta1 + theta2) + I1*l1*m2*r2*theta1_dot^2*sin(theta2) + I2*l1*m2*r2*theta1_dot^2*sin(theta2) + I2*l1*m2*r2*theta2_dot^2*sin(theta2) + g*l1*m1*m2*r2^2*sin(theta1) + g*m1*m2*r1*r2^2*sin(theta1) + 2*l1^2*m2^2*r2^2*theta1_dot*theta2_dot*cos(theta2)*sin(theta2) + g*l1^2*m1*m2*r2*cos(theta2)*sin(theta1) + l1*m1*m2*r1^2*r2*theta1_dot^2*sin(theta2) + 2*I2*l1*m2*r2*theta1_dot*theta2_dot*sin(theta2) + g*l1*m1*m2*r1*r2*cos(theta2)*sin(theta1))/(- l1^2*m2^2*r2^2*cos(theta2)^2 + l1^2*m2^2*r2^2 + I2*l1^2*m2 + m1*m2*r1^2*r2^2 + I1*m2*r2^2 + I2*m1*r1^2 + I1*I2);
 
-X_dot = [x1_dot;x2_dot;x3_dot;x4_dot];
+X_dot = [x1_dot; x2_dot; x3_dot; x4_dot];
 
 A = jacobian(X_dot, x);
 B = jacobian(X_dot, u);
@@ -49,7 +49,6 @@ eigA1 = eig(A) % Unstable at [0,0,0,0]
 %pi,0,0,0
 %0,pi,0,0
 %pi,pi,0,0
-
 
 A2 = jacobian(X_dot,x);
 A2= subs(A2,[theta1,theta2,theta1_dot,theta2_dot],[pi,0,0,0]);
@@ -78,13 +77,14 @@ K = [k11,k12,k13,k14; k21,k22,k23,k24];
 % Acl = A -(B*K);
 % AclPoly = simplify(det(Acl - lambda*eye(4)))
 
-lambda = [-2, -4, -1 - 1i, -1 + 1i];
+lambda = [-1, -2, -1 - 1i, -1 + 1i];
 Kvalues = place(A, B, lambda)
 
 % Question f.
 T = 10;
 [t,y] = ode45(@ode_link, [0,T], [pi/6,pi/4,0,0]);
-K = [24.7907,12.7142,5.7498,6.0241; 6.3156,7.4114,1.7584,2.1889];
+% K = [24.7907,12.7142,5.7498,6.0241; 6.3156,7.4114,1.7584,2.1889];
+K = [23.5850,5.8875,5.1470,2.6108; 5.8875,4.9875,1.5443,0.9770];
 for i = 1:size(y,1)
     u(i)= -(K(1,1)*y(i,1) + K(1,2)*y(i,2)+ K(1,3)*y(i,3) + K(1,4)*y(i,4));
     g(i)= -(K(2,1)*y(i,1) + K(2,2)*y(i,2)+ K(2,3)*y(i,3) + K(2,4)*y(i,4));
@@ -128,7 +128,7 @@ end
 if abs(theta2)>2*pi
     theta2= mod(theta2,2*pi);
 end
-K = [24.7907,12.7142,5.7498,6.0241; 6.3156,7.4114,1.7584,2.1889];
+K = [23.5850,5.8875,5.1470,2.6108; 5.8875,4.9875,1.5443,0.9770];
 U = -K*[theta1;theta2;theta1_dot;theta2_dot];
 
 u1 = [U(1,:)];
